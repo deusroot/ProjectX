67f5cd4b2d54cc210bd3908b8431aa9c6b8bb6c5
amilind <amilind@LH7U05CG6375G13.ms.ds.uhc.com>
2017-10-13 12:15:48 +0530

    first commit

---
+++ src/main/java/LearningJenkins/ProjectX/Learning.java
@@ -0,0 +1,13 @@
+package LearningJenkins.ProjectX;
+
+public class Learning {
+
+	public int add(int x, int y)
+	{
+		return (x+y);
+	}
+	public String concatenation(String x, String y)
+	{
+		return (x+y);
+	}
+}
---
+++ src/test/java/LearningJenkins/ProjectX/AddingTests.java
@@ -0,0 +1,21 @@
+package LearningJenkins.ProjectX;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class AddingTests {
+
+	@Test
+	public void AddTest1()
+	{
+		Learning obj=new Learning();
+		int res=obj.add(50,70);
+		assertEquals(120,res);
+	}
+	@Test
+	public void AddTest2()
+	{
+		Learning obj=new Learning();
+		String res=obj.concatenation("Hello ", "World!");
+		assertEquals("Hello World!",res);
+	}
+}
